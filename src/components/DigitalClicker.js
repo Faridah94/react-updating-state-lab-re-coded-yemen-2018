4488a59da74744cfeb771911d4d6627175bb2f86
Farida <faridah.absi@gmail.com>
2018-10-29 19:50:29 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -2,7 +2,11 @@
 
 import React from 'react';
 
+<<<<<<< HEAD
 class DigitalClicker extends React.Component {
+=======
+class ClickityClick extends React.Component {
+>>>>>>> a35a89675afff4f78476b4c2cff82b18c00ec51b
   constructor() {
     super();
 
@@ -14,17 +18,30 @@ class DigitalClicker extends React.Component {
 
   handleClick = () => {
   this.setState({
+<<<<<<< HEAD
     timesClicked: this.state.timesClicked + 1
   });
+=======
+    timesClicked: 1
+  }, () => (this.state.timesClicked))
+>>>>>>> a35a89675afff4f78476b4c2cff82b18c00ec51b
 }
 
   render() {
     return (
       <div>
+<<<<<<< HEAD
         <button onClick = {this.handleClick}>{this.state.timesClicked}</button>
+=======
+        <button onClick= {this.handleClick}>{this.state.timesClicked}</button>
+>>>>>>> a35a89675afff4f78476b4c2cff82b18c00ec51b
       </div>
     );
   }
 }
 
+<<<<<<< HEAD
 export default DigitalClicker;
+=======
+export default ClickityClick;
+>>>>>>> a35a89675afff4f78476b4c2cff82b18c00ec51b
